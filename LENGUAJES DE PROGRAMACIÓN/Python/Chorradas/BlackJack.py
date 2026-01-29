03da9df5c91862fbf57fb9c9df789492078316b4
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-01-29 17:55:06 +0100

    BlackJack en python (Otra chorrada mia)

---
+++ LENGUAJES DE PROGRAMACIÓN/Python/Chorradas/BlackJack.py
@@ -0,0 +1,46 @@
+import random
+
+def pedirCarta(): 
+    return random.randint(1,11)
+
+def ronda(jugado):
+    sumadoJugador = pedirCarta()
+    sumadoCrupier = pedirCarta()
+    while sumadoJugador <= 21  and sumadoCrupier <=21: 
+        print("Tu puntuacion es: "+str(sumadoJugador))
+        decision = int(input("1: pedir una carta\n 2: plantarse"))
+        if decision == 2: 
+            break 
+        else: 
+            sumadoCrupier += pedirCarta()
+            sumadoJugador +=pedirCarta()
+    
+    if sumadoJugador <= 21: 
+        if sumadoJugador == sumadoCrupier: 
+            print("Empate, recuperas el importe apostado")
+            return jugado
+        elif sumadoJugador > sumadoCrupier or sumadoCrupier > 21: 
+            print("Ganaste")
+            return jugado * 2
+        else: 
+            print("La banca gana, la apuesta se pierde")
+            return 0
+    else: 
+        print("Has perdido, te pasaste de 21")
+def juego(saldo):
+    while saldo > 0: 
+        apuesta = int(input("Empieza la ronda, establece una apuesta o -1 para salir: "))
+        if saldo == -1: 
+            break
+        if apuesta > saldo: 
+            print("Fondos insuficientes")
+        saldo -= apuesta
+        apuesta = ronda(apuesta)
+        saldo += apuesta
+
+def main(): 
+    saldoInicial = int(input("Cuanto dinero deseas jugar"))
+    juego(saldoInicial)
+
+if __name__ == "__main__": 
+    main()
