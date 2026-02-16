672fd6b69a8ff4eaf93167a89d43400ab242b630
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-02-16 17:30:31 +0100

    Agregar animación de número random

--- LENGUAJES DE PROGRAMACIÓN/Python/Ideas Propias/Dado.py
+++ LENGUAJES DE PROGRAMACIÓN/Python/Ideas Propias/Dado.py
@@ -2,12 +2,17 @@ import random
 import time
 def main(): 
     opcion = int(input("¿Cuantos dados vás a tirar?: "))
-    total = 0; 
+    total = 0
     for i in range(0, opcion):
         salida = random.randint(1, 6)
+        duracion = 0.8
+        fin_tiempo = time.time() + duracion
+        while time.time() < fin_tiempo: 
+            azar = random.randint(1,6)
+            print(f"{azar}", end = "\r", flush = True)
+            time.sleep(0.08)
         print(salida)
         total += salida
-        time.sleep(0.3)
     print("Has sacado un: "+ str(total))
 if __name__ == "__main__": 
     main()
