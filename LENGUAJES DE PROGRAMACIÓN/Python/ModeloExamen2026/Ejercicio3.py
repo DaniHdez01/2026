2fb052902f36559cbf2cd9f596bdc00359a2c624
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-03-13 11:17:55 +0100

    Mejorado el ejercicio 3 del modelo de examen

--- LENGUAJES DE PROGRAMACIÓN/Python/ModeloExamen2026/Ejercicio3.py
+++ LENGUAJES DE PROGRAMACIÓN/Python/ModeloExamen2026/Ejercicio3.py
@@ -1,9 +1,7 @@
 def generar_pares(n : int): 
-    a = 0
-    for _ in range (0,n): 
-        if a % 2 == 0: 
-            yield a
-        a += 1
+    for i in range (0,n): 
+        if i % 2 == 0: 
+            yield i
 def main(): 
     n = int(input())
     lista = list(generar_pares(n))
