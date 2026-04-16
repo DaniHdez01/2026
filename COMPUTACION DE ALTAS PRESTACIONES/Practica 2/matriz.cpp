d00550be37019b61dad8668c32a9ec127c974481
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-04-16 11:17:56 +0200

    Quitar struct para evitar cosas raras en memoria

--- COMPUTACION DE ALTAS PRESTACIONES/Practica 2/matriz.cpp
+++ COMPUTACION DE ALTAS PRESTACIONES/Practica 2/matriz.cpp
@@ -2,29 +2,24 @@
 #include <cstdio>
 #include <vector> // Necesario para std::vector
 
-struct Matriz{
-    int rows, cols; 
-    std::vector<int> celdas; // Usar std::vector para un array dinámico
-}; 
+ 
 int main(){
-    Matriz miMatriz; 
-    miMatriz.rows = 3; 
-    miMatriz.cols = 3; 
-    int celds = miMatriz.rows * miMatriz.cols; 
+    int rows = 3; 
+    int cols = 3; 
+    int celds = rows * cols; 
+    std::vector<int> celdas;
 
-    // Redimensionar el vector para que tenga el tamaño correcto
-    miMatriz.celdas.resize(celds);
 
     printf("Número total de celdas: %d\n", celds); 
     for(int i = 0; i< celds; i++){
-        miMatriz.celdas[i] = 1; 
-        printf("Celda: %d Valor %d\n", i, miMatriz.celdas[i]); 
-        int up = i -miMatriz.cols; 
-        int dwn = i + miMatriz.cols; 
+        celdas[i] = 1; 
+        printf("Celda: %d Valor %d\n", i, celdas[i]); 
+        int up = i - cols; 
+        int dwn = i + cols; 
         int lft = i - 1; 
         int rgt = i + 1; 
         printf("Celdas adyacentes: Arriba: %d, Abajo: %d, derecha: %d, izquierda: %d \n ", up, dwn, lft, rgt); 
     }
-    printf("Matriz de %d x %d\n", miMatriz.rows, miMatriz.cols);
+    printf("Matriz de %d x %d\n", rows, cols);
     return 0; // main debe devolver 0 para una ejecución exitosa
 }
