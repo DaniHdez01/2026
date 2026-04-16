8449471d76d6bc47159281f9553e94df2077e7e8
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-04-16 12:19:59 +0200

    Aplicar doble buffer

--- COMPUTACION DE ALTAS PRESTACIONES/Practica 2/matriz.cpp
+++ COMPUTACION DE ALTAS PRESTACIONES/Practica 2/matriz.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <cstdio>
 #include <vector> // Necesario para std::vector
-
+#include <time.h>
  
+#include <cstdlib> // Para malloc, free, rand, srand
+#include <cstring> // Para memcpy
+
 int main(){
     int rows = 3; 
     int cols = 3; 
     int celds = rows * cols; 
-    std::vector<int> celdas;
+    double *arr_old = (double*) malloc(celds * sizeof(double));
+    double *arr_new = (double*) malloc(celds * sizeof(double));
 
+    // Comprobar si la asignación de memoria fue exitosa
+    if (arr_old == NULL || arr_new == NULL) {
+        fprintf(stderr, "Error: Fallo en la asignación de memoria.\n");
+        return 1; // Indicar un error
+    }
 
+    srand(time(NULL)); 
+
+    //AHORA INICIALIZAMOS EL ARRAY ANTIGUO
     printf("Número total de celdas: %d\n", celds); 
     for(int i = 0; i< celds; i++){
-        celdas[i] = 1; 
-        printf("Celda: %d Valor %d\n", i, celdas[i]); 
-        int up = i - cols; 
-        int dwn = i + cols; 
-        int lft = i - 1; 
-        int rgt = i + 1; 
-        printf("Celdas adyacentes: Arriba: %d, Abajo: %d, derecha: %d, izquierda: %d \n ", up, dwn, lft, rgt); 
+        arr_old[i] =  (double)(rand() % 30 + 1); // Castear a double
+        printf("Temperatura inicial de la celda %d: %.2f \n", i, arr_old[i]);
     }
-    printf("Matriz de %d x %d\n", rows, cols);
+
+    memcpy(arr_new, arr_old, celds * sizeof(double));
+    for(int i = 1; i < rows -1; ++i){
+        for(int j = 1; j < cols - 1; ++j){
+            int idx = i * cols + j;
+            int up = (i-1) * cols + j; 
+            int dwn  = (i + 1) * cols + j;
+            int lft    = i * cols + (j - 1);
+            int rgt    = i * cols + (j + 1);
+            arr_new[idx] = (arr_old[up] + arr_old[dwn] + arr_old[lft] + arr_old[rgt]) / 4.0;
+        }
+
+    }
+    double *temp = arr_old; 
+    arr_old = arr_new;
+    arr_new = temp;
+    for(int i = 0; i<celds; i++){
+        printf("celda %d, Temperatura: %.2f \n", i, arr_old[i]); 
+    }
+    free(arr_old); 
+    free(arr_new); 
     return 0; // main debe devolver 0 para una ejecución exitosa
 }
