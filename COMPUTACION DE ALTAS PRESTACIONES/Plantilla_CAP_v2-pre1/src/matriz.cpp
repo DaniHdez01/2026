d7e71b2675bce73eba8fb7b73fb5d79a15eeab49
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-04-28 13:54:27 +0200

    CAP: Comparación entre sharing dinámico y estático

--- COMPUTACION DE ALTAS PRESTACIONES/Plantilla_CAP_v2-pre1/src/matriz.cpp
+++ COMPUTACION DE ALTAS PRESTACIONES/Plantilla_CAP_v2-pre1/src/matriz.cpp
@@ -1,9 +1,9 @@
 #include <cstdio>
 #include <cstdlib> // Para malloc, free, rand, srand
 #include <cstring> // Para memcpy
+#include <ctime>
 #include <iostream>
 #include <omp.h>
-#include <time.h>
 #include <vector> // Necesario para std::vector
 
 int main() {
@@ -40,25 +40,30 @@ int main() {
     printf("Indique el número de iteraciones para simular: ");
     std::cin >> its;
     memcpy(arr_new, arr_old, celds * sizeof(double));
-// BUCLES CRÍTICOS PARA PARALELIZAR
-/**
- * COSAS A TENER EN CUENTA:
- * 1. Qué se puede paralelizar?
- * 2. Qué variables van a compartir?
- *      1. Los punteros a los arreglos +ç
- *      2. La variable N que indica el tamaño de estos
- * 3. Qué variables deben ser privadas
- */
-#pragma omp paralell
+    // BUCLES CRÍTICOS PARA PARALELIZAR
+    /**
+     * COSAS A TENER EN CUENTA:
+     * 1. Qué se puede paralelizar?
+     * 2. Qué variables van a compartir?
+     *      1. Los punteros a los arreglos
+     *      2. La variable N que indica el tamaño de estos
+     * 3. Qué variables deben ser privadas
+     */
+    unsigned t0, t1;
+    t0 = clock();
+#pragma omp paralell // Iniciar el entorno donde vamos a paralelizar
     {
         for (int i = 0; i < its; i++) { // BUCLE DE LAS IITERACIONES: IMPARALELIZABLE
 
-// Indicamos que se cree un equipo de hilos y se reparta el bucle for
-// Usamos schedule(static) porque todas las celdas tardan lo mismo en calcularse El enunciado pide que
-// comparemos el estático con el dinámico
-#pragma omp parallel for schedule(static) shared(T_old, T_new, N)
+            // Indicamos que se cree un equipo de hilos y se reparta el bucle for
+            // Usamos schedule(static) porque todas las celdas tardan lo mismo en calcularse El enunciado pide que
+            // comparemos el estático con el dinámico
+// #pragma omp parallel for schedule(static) shared(arr_old, arr_new, N)
+#pragma omp parallel for schedule(dynamic)
             for (int i = 1; i < rows - 1; ++i) {     // BUCLE DE FILAS
                 for (int j = 1; j < cols - 1; ++j) { // BUCLE DE COLUMNAS
+                    // Por qué no podemos paralelizar este for? -> Se produce Fasle Sharing ya que los datos en memoria
+                    // son contíguos a diferencia del bucle anterior
 
                     // CÓMPUTO
                     int idx = i * cols + j;
@@ -77,9 +82,12 @@ int main() {
             }
         }
     }
+    t1 = clock();
+    double time = (double(t1 - t0) / CLOCKS_PER_SEC);
     for (int i = 0; i < celds; i++) {
         printf("celda %d, Temperatura: %.2f \n", i, arr_old[i]);
     }
+    std::cout << "Tiempo de ejecución: " << time << " segundos" << std::endl;
     free(arr_old);
     free(arr_new);
     return 0; // main debe devolver 0 para una ejecución exitosa
